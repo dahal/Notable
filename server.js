e084294a4bc397a57848d0b3e5040f749d6148e2
Puru Dahal <pdahal@me.com>
2014-10-10 16:55:12 -0700

    Define CRUD routes

--- server.js
+++ server.js
@@ -2,3 +2,12 @@
 var koa   = require('koa'),
     route = require('koa-route'),
     app = koa()
+
+// Define the routes
+app.use(route.get('/', notes));
+app.use(route.get('/note/new', add));
+app.use(route.get('/note/:id', show));
+app.use(route.get('/note/delete/:id', remote));
+app.use(route.get('/note/edit/:id', edit));
+app.use(route.post('/note/create', create));
+app.use(route.post('/todo/update', update))
