75a6bd57b9dcb4a841168577629919fbae3ed27a
Puru Dahal <pdahal@me.com>
2014-10-10 17:48:36 -0700

    Add views, layout, co-views for rendering views and load server on port 8080

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 // Require dependencies
 var koa   = require('koa'),
     route = require('koa-route'),
-    app = koa()
+    app   = koa(),
+    views = require('co-views') 
 
 // Define the routes
-app.use(route.get('/', notes));
-app.use(route.get('/note/new', add));
-app.use(route.get('/note/:id', show));
-app.use(route.get('/note/delete/:id', remote));
-app.use(route.get('/note/edit/:id', edit));
-app.use(route.post('/note/create', create));
-app.use(route.post('/todo/update', update))
+// app.use(route.get('/', notes));
+// app.use(route.get('/note/new', add));
+// app.use(route.get('/note/:id', show));
+// app.use(route.get('/note/delete/:id', remote));
+// app.use(route.get('/note/edit/:id', edit));
+// app.use(route.post('/note/create', create));
+// app.use(route.post('/todo/update', update))
+
+// Spicify views folder and swig for template
+var render = views(__dirname + '/views', {map: {html: 'swig'}})
+
+// Start the server
+app.listen(8080);
+console.log('Notable loading on: http://localhost:8080')
